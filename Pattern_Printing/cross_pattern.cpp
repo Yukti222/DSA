33dd7743c527d42046b04cce0d948b26fc627bf8
Yukti222 <idea22122@gmail.com>
2026-09-03 22:31:16 +0530

    triangle_pattern

---
+++ Pattern_Printing/cross_pattern.cpp
@@ -0,0 +1,21 @@
+// *       *
+//  *    *
+//     *
+//  *     *
+
+#include <iostream>
+using namespace std;
+int main(){
+  int m;
+  cout << "enter a number";
+  cin>> m;
+  for (int i =1; i<=m ; i++){
+    for (int j=1; j<=m; j++){
+      if(i==j|| j== (m-i+1)){
+        cout << "* ";
+      }
+      else cout << " ";
+    }
+    cout << endl;
+  }
+}
