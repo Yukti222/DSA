6aa40cfbedc21526fc001cdd6128e6d8c86b17fe
Yukti222 <idea22122@gmail.com>
2026-09-06 21:28:01 +0530

    time_complexity

---
+++ Time_Complexity/theory_algo.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// what is time complexity--
+// Rate at which the time taken moves as it with respect to the input size;
+// denoted by bigo(o) notation
+
+ 
+// points to remeber while calculating time complexity
+ // 1. Ignore the constant factors
+ // 2. Ignore the lower order terms
+ // 3. Consider the worst case scenario
+
+
+ // three types of time complexity
+ // big-oh (0) - upper bound
+ // big-omega (Ω) - lower bound
+ // big-theta (Θ) - tight bound
+
+ // 
